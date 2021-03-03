5d38b2383a70291194a63ab9a5aa0e883457342b
algosketch <sjy_9484@naver.com>
2021-03-03 19:50:27 +0900

    과제 2차

--- app/src/main/java/com/inu/study/MainActivity.kt
+++ app/src/main/java/com/inu/study/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.inu.study
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            val id = findViewById<EditText>(R.id.input_id)
+            val pw = findViewById<EditText>(R.id.input_password)
+            if(id.text.toString().isEmpty() || pw.text.toString().isEmpty()) {
+                Toast.makeText(this, "아이디 혹은 비밀번호를 입력하지 않았습니다.", Toast.LENGTH_SHORT).show()
+                if(id.text.toString().isEmpty()) {
+                    id.requestFocus()
+                } else {
+                    pw.requestFocus()
+                }
+            }
+            else findViewById<TextView>(R.id.error_message).visibility = View.VISIBLE
+        }
     }
 }
+
---
+++ app/src/main/java/com/inu/study/SubActivity.kt
@@ -0,0 +1,14 @@
+package com.inu.study
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
