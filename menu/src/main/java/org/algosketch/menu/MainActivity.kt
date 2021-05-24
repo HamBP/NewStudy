588a840e3f98023853dfc3fe95de9ee398cd84ec
algosketch <sjy_9484@naver.com>
2021-05-25 03:05:49 +0900

    툴바

--- menu/src/main/java/org/algosketch/menu/MainActivity.kt
+++ menu/src/main/java/org/algosketch/menu/MainActivity.kt
@@ -6,25 +6,35 @@ import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     lateinit var drawer: DrawerLayout
-    var isDrawerOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val drawerButton = findViewById<ImageButton>(R.id.btn_drawer)
+        drawerButton.setOnClickListener {
+            drawer.openDrawer(Gravity.LEFT)
+        }
 
         drawer = findViewById(R.id.drawer)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
-        //inflater.inflate(R.menu.menu1, menu)
-        inflater.inflate(R.menu.drawer, menu)
+        inflater.inflate(R.menu.menu1, menu)
         return true
     }
 
@@ -36,12 +46,6 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.item2 -> {
                 Toast.makeText(this, "item2 menu clicked!", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.btn_drawer -> {
-                isDrawerOpen = !isDrawerOpen
-                if(isDrawerOpen) drawer.openDrawer(Gravity.LEFT)
-                else drawer.closeDrawer(Gravity.LEFT)
                 true
             }
             else -> super.onOptionsItemSelected(item)
